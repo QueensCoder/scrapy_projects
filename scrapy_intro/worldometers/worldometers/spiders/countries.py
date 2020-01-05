617aebcf394a96a6875ad636c2a068807e3a133f
Ozal Khan <ozalkhan196@gmail.com>
2020-01-05 14:16:27 -0500

    able to visit every link on the worldmeters site

--- scrapy_intro/worldometers/worldometers/spiders/countries.py
+++ scrapy_intro/worldometers/worldometers/spiders/countries.py
@@ -4,15 +4,18 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
+    allowed_domains = ['www.worldometers.info']
     start_urls = [
         'https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
+        countries = response.xpath('//td/a')
 
-        yield {
-            'title': title,
-            "countries": countries
-        }
+        for i in countries:
+            # ./ because we are not using xpath agaisnt the response obj anymore
+            name = i.xpath(".//text()").get()
+            link = i.xpath(".//@href").get()
+
+            # to deal with relative urls
+            absolute_url = f"https://www.worldometers.info{link}"
+            yield scrapy.Request(url=absolute_url)
