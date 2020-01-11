da861f5ce198c9c8d868f432974212bb807b8097
Ozal Khan <ozalkhan196@gmail.com>
2020-01-11 18:52:06 -0500

    added debugger runner file for using vscode debugger for scrapy

--- scrapy_intro/worldometers/worldometers/spiders/countries.py
+++ scrapy_intro/worldometers/worldometers/spiders/countries.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import logging
+from scrapy.shell import inspect_response
 
 
 class CountriesSpider(scrapy.Spider):
@@ -13,28 +14,31 @@ class CountriesSpider(scrapy.Spider):
     def parse(self, response):
         countries = response.xpath('//td/a')
 
-        for i in countries:
-            # ./ because we are not using xpath agaisnt the response obj anymore
-            name = i.xpath(".//text()").get()
-            link = i.xpath(".//@href").get()
+        # for i in countries:
+        #     # ./ because we are not using xpath agaisnt the response obj anymore
+        #     name = i.xpath(".//text()").get()
+        #     link = i.xpath(".//@href").get()
 
-            # to deal with relative urls
+        # to deal with relative urls
 
-            # manually concat url
-            # absolute_url = f"https://www.worldometers.info{link}"
+        # manually concat url
+        # absolute_url = f"https://www.worldometers.info{link}"
 
-            # url join is cleaner way to concat url with links
-            # absolute_url = response.urljoin(link)
+        # url join is cleaner way to concat url with links
+        # absolute_url = response.urljoin(link)
 
-            # use with string concat methods above
-            # yield scrapy.Request(url=absolute_url)
+        # use with string concat methods above
+        # yield scrapy.Request(url=absolute_url)
 
-            # do not need concat methods above when using response.follow
-            yield response.follow(url=link, callback=self.parse_country, meta={'country_name': name})
-            # meta allows us to pass the country name from method to method
+        # do not need concat methods above when using response.follow
+        yield response.follow(url="https://www.worldometers.info/world-population/china-population/", callback=self.parse_country, meta={'country_name': "China"})
+        # meta allows us to pass the country name from method to method
 
     # for each country get the year and population data
     def parse_country(self, response):
+
+        # opens shell so you can inspect the req and res
+        # inspect_response(response, self)
 
         # get name from the response
         name = response.request.meta['country_name']
