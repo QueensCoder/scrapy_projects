3541fb769710345872596479c46d023691c54d08
Ozal Khan <ozalkhan196@gmail.com>
2020-01-23 12:56:35 -0500

    able to scrap all top 250 movies with pagination using rules and link extractors

--- scrapy_intro/imbd/imbd/settings.py
+++ scrapy_intro/imbd/imbd/settings.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 # Scrapy settings for imbd project
 #
 # For simplicity, this file contains only settings considered important or
@@ -39,34 +40,34 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'imbd.middlewares.ImbdSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'imbd.middlewares.ImbdDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'imbd.pipelines.ImbdPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- scrapy_intro/imbd/imbd/spiders/best_movies.py
+++ scrapy_intro/imbd/imbd/spiders/best_movies.py
@@ -7,13 +7,33 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['imdb.com']
-    start_urls = ['https://www.imdb.com/chart/top/?ref_=nv_mv_250']
+    start_urls = [
+        'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating']
 
+    # link extractor takes out the link from the xpath provided and passed that link to the callback method
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=r"//td[@class='titleColumn']/a"),
+        Rule(LinkExtractor(restrict_xpaths=r"//h3[@class='lister-item-header']/a"),
              callback='parse_item', follow=True),
-        # , after rule is required or spider will crash
+        Rule(LinkExtractor(
+            restrict_xpaths="(//a[@class='lister-page-next next-page'])[1]"))
+        # , after rule is required or spider will crash if one rule
+        # rules are invoked in the order that they are written in
+        # first get all the links from the first page, then use pagination
+        # for second rule the default value for follow is true
+        # each time a new page is visited the first rule will be called again so no need to use call back of parse_item in second rule
     )
 
     def parse_item(self, response):
-        print(response.url)
+        yield {
+            'title': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'year': response.xpath("//span[@id='titleYear']/a/text()").get(),
+
+            # normalize-space removes whitespace and \n inside of output text
+            'duration': response.xpath("normalize-space(//div[@class='subtext']/time/text())").get(),
+            'rating': response.xpath("//div[@class='ratingValue']/strong/span/text()").get(),
+            'movie_url': response.xpath("//div[@class='title_wrapper']/h1/text()").get(),
+            'genre': response.xpath("//div[@class='subtext']/a[1]/text()").get(),
+            'movie_url': response.url
+
+
+        }
