d027c2c107598c2e91796d923881f3ce398b44c7
Ozal Khan <ozalkhan196@gmail.com>
2020-02-18 21:20:51 -0500

    set up driver to crawl page using selenium and scrapy, had to make screen max resolution size in order to scrape all desired elements

--- scrapy_intro/selenium_basics/basics.py
+++ scrapy_intro/selenium_basics/basics.py
@@ -1,17 +1,32 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 from shutil import which
 
+# allows you to perform a headless scrape (does not open the browser)
+chrome_options = Options()
+chrome_options.add_argument('--headless')
 
 # which handles the path for us
 # had to add chrome driver to path ---will need to update if new version of chrome used
 chrome_path = which('chromedriver')
 
 
-driver = webdriver.Chrome(executable_path=chrome_path)
+driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 driver.get('https://duckduckgo.com')
 
 search = driver.find_element_by_id('search_form_input_homepage')
 search.send_keys('My User Agent')
 
-btn = driver.find_element_by_id("search_button_homepage")
-btn.click()
+# click search btn by first finding button by id
+# btn = driver.find_element_by_id("search_button_homepage")
+# btn.click()
+
+# USE KEYS.ENTER TO PEFORM SEARCH
+search.send_keys(Keys.ENTER)
+
+# print page source to terminal
+print(driver.page_source)
+
+# make sure to close driver
+driver.close()
