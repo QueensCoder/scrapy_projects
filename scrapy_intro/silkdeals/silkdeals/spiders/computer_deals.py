d34210d6daa4d7e421f7a3bde83583361f26377d
Ozal Khan <ozalkhan196@gmail.com>
2020-02-19 11:04:42 -0500

    able to get all items from first page of slick deals using selenium, scrapy and xpath

---
+++ scrapy_intro/silkdeals/silkdeals/spiders/computer_deals.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy_selenium import SeleniumRequest
+
+
+class ComputerDealsSpider(scrapy.Spider):
+    name = 'computer_deals'
+
+    def start_requests(self):
+        yield SeleniumRequest(
+            url='https://slickdeals.net/computer-deals/',
+            wait_time=3,
+            screenshot=True,
+            callback=self.parse
+        )
+
+    def parse(self, response):
+        products = response.xpath(
+            "//ul[@class='dealTiles categoryGridDeals']/li")
+
+        for prod in products:
+            yield {
+                'name': prod.xpath(".//a[@class='itemTitle']/text()").get(),
+                'link': prod.xpath(".//a[@class='itemTitle']/@href").get(),
+                'store': prod.xpath(".//span[@class='itemStore']/text()").get(),
+                'price': prod.xpath(".//div[@class='itemPrice  wide ']/text()").get()
+            }
